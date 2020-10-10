164112fdc5dc58ae1466c4419862af24a04b4cac
claudiumocanu <neo.ergo.sum@gmail.com>
2020-10-10 12:41:39 +0300

    frontend initial draft

---
+++ acm-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SensorsComponent } from './sensors/sensors.component';
+
+const routes: Routes = [
+  {
+    path:'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path:'sensors',
+    component: SensorsComponent
+  },
+  {
+    path:'admin',
+    component: AdminComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ acm-frontend/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RootNavComponent } from './root-nav/root-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SensorsComponent } from './sensors/sensors.component';
+import { AdminComponent } from './admin/admin.component';
+import { GoogleChartsModule } from 'angular-google-charts';
+
+import { MatTableModule } from '@angular/material/table'  
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RootNavComponent,
+    DashboardComponent,
+    SensorsComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    GoogleChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
